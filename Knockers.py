5fc3ac339d0b89162145c3eb967d36a4ee322bf7
Tabby <ba0c0onn@gmail.com>
2025-02-02 00:01:33 +0300

    Update Knockers.py

--- Knockers.py
+++ Knockers.py
@@ -61,16 +61,16 @@ def update(dt):
             dash_to_cursor()
             dash_cooldown = dash_cooldown_max
 
-        # Rock throw (Left Click)
+        # Rock throw (Q key)
         if rock_clicks < rock_click_limit and rock_cooldown <= 0:
-            if keys[key.L]:
+            if keys[key.Q]:
                 throw_rock()
                 rock_clicks += 1
                 if rock_clicks >= rock_click_limit:
                     rock_cooldown = rock_cooldown_max
 
-        # Boomerang cube (Right Click)
-        if boomerang_cooldown <= 0 and keys[key.R]:
+        # Boomerang cube (E key)
+        if boomerang_cooldown <= 0 and keys[key.E]:
             shoot_boomerang()
             boomerang_cooldown = boomerang_cooldown_max
 
@@ -261,13 +261,4 @@ class Boomerang:
             # Return to player
             dx = player.x - self.shape.x
             dy = player.y - self.shape.y
-            distance = (dx ** 2 + dy ** 2) ** 0.5
-            if distance > 0:
-                self.shape.x += dx / distance * dt * 300
-                self.shape.y += dy / distance * dt * 300
-
-# Schedule updates
-pyglet.clock.schedule_interval(update, 1/120.0)  # 120 FPS cap
-
-# Run the game
-pyglet.app.run()
+            distance = (dx ** 2 + dy **
